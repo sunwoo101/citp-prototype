797ba10f1bca5b0046ae302cf809580293bccc1e
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-10-04 23:40:20 +1000

    [frontend] footer

---
+++ frontend/src/pages/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-right p-4">
+            <p className="text-xl text-red-600 font-bold">© LUCARTI GROUP 2025</p>
+        </footer>
+    )
+}
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer"
+
+export default function Home() {
+    return (
+        <>
+            <main>
+            </main>
+            <Footer />
+        </>
+    )
+}
