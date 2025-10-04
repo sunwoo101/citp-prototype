c59f44ff7aa5968b16b049a7c661e7b80a9ef16a
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-10-05 00:35:47 +1000

    [frontend] footer fixes

--- frontend/src/pages/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="text-right p-4">
-            <p className="text-xl text-red-600 font-bold">© LUCARTI GROUP 2025</p>
-        </footer>
-    )
-}
---
+++ frontend/src/pages/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="p-4">
+            <p className="text-xl text-red-600 font-bold text-right leading-none">© LUCTARI GROUP 2025</p>
+        </footer>
+    )
+}
