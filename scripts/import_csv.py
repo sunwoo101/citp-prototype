193882414943f5a1505cce7c920d95a9c1690e35
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-10-07 00:18:07 +1100

    [script] import all functionality

--- scripts/import_csv.py
+++ scripts/import_csv.py
@@ -9,6 +9,16 @@ CSV_DIR = "csv"
 
 
 def import_words(filename):
+    if filename == "all":
+        csv_files = [f for f in os.listdir(CSV_DIR) if f.endswith(".csv")]
+        if not csv_files:
+            print("❌ No CSV files found in the csv/ directory.")
+            sys.exit(1)
+        for file in csv_files:
+            print(f"\n Importing from {file} ...")
+            import_words(file)
+        return
+
     csv_file = os.path.join(CSV_DIR, filename)
     if not os.path.exists(csv_file):
         print(f"❌ File not found: {filename}")
@@ -40,5 +50,6 @@ def import_words(filename):
 
 if __name__ == "__main__":
     # test()
-    csv_file = input("CSV Name (example a_words.csv): ")
+    csv_file = input(
+        "CSV Name (example a_words.csv or all to import all .csv): ")
     import_words(csv_file)
